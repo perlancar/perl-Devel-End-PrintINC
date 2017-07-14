e882f26423527d72c3468275cdd782b4202357ff
perlancar (on netbook-dell-vostro) <perlancar@gmail.com>
2017-07-14 14:31:05 +0700

    Initial commit

---
+++ lib/Devel/End/PrintINC.pm
@@ -0,0 +1,31 @@
+package Devel::End::PrintINC;
+
+# DATE
+# VERSION
+
+END {
+    print "Contents of \@INC:\n";
+    for (@INC) { print "  $_\n" }
+
+    print "Contents of \%INC:\n";
+    for (sort keys %INC) {
+        print "  $_ ($INC{$_})\n";
+    }
+}
+
+1;
+# ABSTRACT: Print @INC and %INC when program ends
+
+=head1 SYNOPSIS
+
+ % perl -MDevel::End::PrintINC -e'...'
+
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<Devel::DieHandler::PrintINCVersion>
+
+Other C<Devel::End::*> modules
